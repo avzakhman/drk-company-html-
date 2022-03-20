3a83a98c8b7c0d86895cba533a5770e0ade41d59
Alexander <zaharov4625@gmail.com>
2022-03-21 01:21:30 +0300

    image optimisation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,60 +1,72 @@
-let select  = function() {
-    let selectHeader = document.querySelectorAll(".select__header");
-    let selectItem = document.querySelectorAll(".select__item");
-    selectHeader.forEach(item=> {
-        item.addEventListener('click', selectToggle)
-    });
-    selectItem.forEach(item=> {
-        item.addEventListener('click', selectChoose)
-    });
+window.addEventListener('DOMContentLoaded', ()=> {
 
+    //меню выбора в форме
+    const select  = function() {
+        let selectHeader = document.querySelectorAll(".select__header");
+        let selectItem = document.querySelectorAll(".select__item");
+        selectHeader.forEach(item=> {
+            item.addEventListener('click', selectToggle)
+        });
+        selectItem.forEach(item=> {
+            item.addEventListener('click', selectChoose)
+        });
+    
+    
+        function selectToggle() {
+            this.parentElement.classList.toggle('is-active');
+        }
+    
+        function selectChoose() {
+            let text = this.innerText,
+                select = this.closest('.select'),
+                currentText = this.closest('.select').querySelector('.select__current');
+            currentText.innerText = text;
+            select.classList.remove('is-active');
+        }
+    
+    };
+    select();
+    
+    
 
-    function selectToggle() {
-        this.parentElement.classList.toggle('is-active');
+    //функция слайдера
+    const slick = function() {
+        var $slider = $('.fragen__cards');
+        $slider.slick({
+            adaptiveHeight: true,
+            slideShow: true,
+            slidesToShow: 1
+        });
+        
+        $(window).on('load resize', function() {
+            if ($(window).width() < 577) {
+              $('.fragen__cards:not(.slick-initialized)').slick({
+              });
+            } else {
+              $(".fragen__cards.slick-initialized").slick("unslick");
+            }
+        });    
     }
+    slick();
+    
+    
 
-    function selectChoose() {
-        let text = this.innerText,
-            select = this.closest('.select'),
-            currentText = this.closest('.select').querySelector('.select__current');
-        currentText.innerText = text;
-        select.classList.remove('is-active');
+    //функция счётчика
+    const count = function() {
+        var slider 		 = $('.fragen__cards');
+        total        = slider.slick("getSlick").slideCount;
+        currentSlide = $('.fragen__cards').slick('slickCurrentSlide');
+        slide        = currentSlide + 1;
+        if ( total > 1 ) {
+            $(".sl-count__current").text(slide);
+            $('.sl-count__total').text(total);
+        }
+        $(".fragen__cards").on('afterChange', function(event, slick, currentSlide, nextSlide){
+            var currentSl = currentSlide + 1;
+            $(".sl-count__current").text(currentSl);
+        });
     }
-
-};
-
-select();
+    count();
+})
 
 
-
-var $slider = $('.fragen__cards');
-$slider.slick({
-    adaptiveHeight: true,
-    slideShow: true,
-    slidesToShow: 1
-});
-
-$(window).on('load resize', function() {
-    if ($(window).width() < 577) {
-      $('.fragen__cards:not(.slick-initialized)').slick({
-      });
-    } else {
-      $(".fragen__cards.slick-initialized").slick("unslick");
-    }
-});
-
-
-
-
-var slider 		 = $('.fragen__cards');
-	total        = slider.slick("getSlick").slideCount;
-	currentSlide = $('.fragen__cards').slick('slickCurrentSlide');
-	slide        = currentSlide + 1;
-if ( total > 1 ) {
-	$(".sl-count__current").text(slide);
-	$('.sl-count__total').text(total);
-}
-$(".fragen__cards").on('afterChange', function(event, slick, currentSlide, nextSlide){
-	var currentSl = currentSlide + 1;
-	$(".sl-count__current").text(currentSl);
-});
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp         = require('gulp');
 const browserSync  = require('browser-sync');
 const sass         = require('gulp-sass')(require('sass'));
-const cleanCSS     = require('gulp-clean-css');
+// const cleanCSS     = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename       = require("gulp-rename");
 const htmlmin      = require('gulp-htmlmin');
@@ -20,10 +20,10 @@ gulp.task('server', function() {
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        // .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        // .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
